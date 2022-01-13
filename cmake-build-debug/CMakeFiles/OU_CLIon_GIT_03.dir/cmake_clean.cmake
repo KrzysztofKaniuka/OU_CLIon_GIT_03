af490f306dd341945057db75c9c53349048c7beb
NightRaven <240697@edu.p.lodz.pl>
2022-01-13 20:56:20 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLIon_GIT_03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLIon_GIT_03.dir/main.cpp.obj"
+  "OU_CLIon_GIT_03.exe"
+  "OU_CLIon_GIT_03.exe.manifest"
+  "OU_CLIon_GIT_03.pdb"
+  "libOU_CLIon_GIT_03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLIon_GIT_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_CLIon_GIT_03.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_CLIon_GIT_03.
+# This may be replaced when dependencies are built.
